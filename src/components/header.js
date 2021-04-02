c5cfe0e460d330341c1534a8329938fbfc0333ae
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-02 13:04:18 +0530

    new-added onclick event for count incraese

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,14 @@
 import React ,{useState} from "react"
 const Header=()=>{
+    const [count,setCount]=useState(0)
+    // function countHandler(){
+    //     setCount(count+1);
+    // }
     return(
         <>
+        <p>You clicked button {count} times</p>
+        <button onClick={()=>setCount(count+1)} >Count</button>
+        
         </>
     )
 }
