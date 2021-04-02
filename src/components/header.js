5a4d9f6d25e5b13ec0dece86d80dc919918497d1
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-02 13:05:31 +0530

    fix-counthandler

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,13 @@
 import React ,{useState} from "react"
 const Header=()=>{
     const [count,setCount]=useState(0)
-    // function countHandler(){
-    //     setCount(count+1);
-    // }
+    function countHandler(){
+        setCount(count+1);
+    }
     return(
         <>
         <p>You clicked button {count} times</p>
-        <button onClick={()=>setCount(count+1)} >Count</button>
+        <button onClick={()=>{countHandler()}} >Count</button>
         
         </>
     )
